b69ec7a5e35685981718468686273d84b242d145
AidenDaniel <aidendaniel2708@gmail.com>
2021-10-13 14:54:44 +0700

    BTVN_1

---
+++ BTVN_1/bai_1/runmain.java
@@ -0,0 +1,18 @@
+package bai_1;
+
+public class runmain {
+
+  public static void main (String[] args)
+  {
+    int n=4, m=6;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            if(i == 1 || i == n || j == 1 || j == m)
+                System.out.print("*");
+            else 
+                System.out.print(" ");
+        }
+        System.out.print("\n");
+    }
+  }
+}
---
+++ BTVN_1/bai_2/runmain.java
@@ -0,0 +1,23 @@
+package bai_2;
+
+public class runmain {
+    public static void main(String[] args) {
+        System.out.println("*       *     *     * * * *   * * * *   *   *");
+        System.out.println("|       |    / \\    |      *  |      *   \\ /");
+        System.out.println("* ----- *   *---*   * * * *   * * * *     *");
+        System.out.println("|       |  /     \\  |         |           |");
+        System.out.println("*       * *       * *         *           *");
+        System.out.println("");
+        System.out.println("*    *  ******  *       *       *");
+        System.out.println("| \\  |  |        \\     / \\     /");
+        System.out.println("*  * *  ******    *   *   *   *");
+        System.out.println("|   \\|  |          \\ /     \\ /");
+        System.out.println("*    *  ******      *       *");
+        System.out.println("");
+        System.out.println("*   *  ******      *     * * * *");
+        System.out.println(" \\ /   |          / \\    |      *");
+        System.out.println("  *    ******    *---*   * * * *");
+        System.out.println("  |    |        /     \\  |    \\");
+        System.out.println("  *    ******  *       * *      *");
+    }
+}
